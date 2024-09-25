8f782ecc05742542854d2e046dfa1e97611525cf
Jules Schoonman <jules.schoonman@gmail.com>
2024-09-25 15:17:24 +0200

    added maps

--- src/data/maps.json.js
+++ src/data/maps.json.js
@@ -8,6 +8,14 @@ const data = [
         "url": "https://www.nationaalarchief.nl/onderzoeken/archief/4.DEF/invnr/1.7/file/NL-HaNA_4.DEF_1.7"
     },
     {
+        "title": "'t Hooge Heemraedschap van Delflant",
+        "year": "1712",
+        "manifest": "https://dlc.services/iiif-resource/7/string1string2string3/1712-kruikius",
+        "annotation": "https://annotations.allmaps.org/maps/c042ba90d01f6dd0@387944b87ef79000",
+        "collection": "TU Delft Library",
+        "url": "https://www.tudelft.nl/library/collecties/kaartenkamer/kaartencollectie/historische-kaarten/kruikiuskaart-1712"
+    },
+    {
         "title": "S.W. van der Noordaa, Nieuwe Kaart van s' Gravenhage met de omliggende Dorpen en Buitenplaatesen",
         "year": 1852,
         "manifest": "https://service.archief.nl/iipsrv?IIIF=/d3/05/c6/2a/3a/68/4d/d0/8d/af/2e/d7/68/0f/18/b2/df222597-90a3-4724-a043-bfc80c726fed.jp2/info.json",
@@ -43,9 +51,17 @@ const data = [
         "title": "Tramgids van 's-Gravenhage",
         "year": "1926",
         "manifest": "https://images.memorix.nl/hga/iiif/73216a2a-9a1b-c711-1c7b-67adfd106f78",
-        "annotation": "https://annotations.allmaps.org/images/47bacb78da90dff2",
+        "annotation": "https://annotations.allmaps.org/maps/a50c244ce62b7fe3@6b595e06095a35e5",
         "collection": "Haags Gemeentearchief",
         "url": "https://haagsgemeentearchief.nl/mediabank/beeldcollectie/detail/e9cce592-7101-aa99-79e4-760ef6acabc5/media/73216a2a-9a1b-c711-1c7b-67adfd106f78"
+    },
+    {
+        "title": "Plattegrond 's-Gravenhage met tram- en buslijnen",
+        "year": "1975",
+        "manifest": "https://images.memorix.nl/hga/iiif/afff62ea-7439-37ed-f600-7ccb3ed4cb67",
+        "annotation": "https://annotations.allmaps.org/maps/9d55ea0c8176b2d4@6183adc21af24185",
+        "collection": "Haags Gemeentearchief",
+        "url": "https://haagsgemeentearchief.nl/mediabank/beeldcollectie/detail/1365f322-31a2-45e0-895a-ceb046c2e69c/media/afff62ea-7439-37ed-f600-7ccb3ed4cb67"
     }
 ]
 
